8e606af77131e214849ac81044d1aff7216ff48a
jmal <zhushilun084@gmail.com>
2024-10-25 10:53:36 +0800

    Add Analytics

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -1,12 +1,22 @@
 import { withMermaid } from 'vitepress-plugin-mermaid'
 
-import { en } from './en'
 import { cn, cnSearch } from './cn'
+import { en } from './en'
 
 export default withMermaid({
   title: 'JmalCloud',
   base: '/',
   lastUpdated: true,
+  head: [
+    [
+      'script',
+      {
+        'defer': true,
+        'src': 'https://umami.jmal.top/script.js',
+        'data-website-id': 'cf91210b-8bf1-4986-9222-93988afb9152',
+      },
+    ],
+  ],
   themeConfig: {
     logo: '/logo.svg',
     socialLinks: [
